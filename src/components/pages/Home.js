dd905c5a0eec8b9b5e00655cc8cc93c4d31560b1
Edinaldo Fernandes Cardoso Santos <fernandescro5@gmail.com>
2022-02-11 13:40:17 -0300

    Aula20-home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,17 @@
-
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import BtnLink from '../layout/BtnLink';
 
 function Home(){
 
     return(
-        <h1>
-            Home
-        </h1>
+        <section className={styles.homeContainer}>
+            <h1> Bem vindo ao <span>costs</span></h1>
+            <p>Comece a gerenciar seus projetos agora mesmo</p>
+            <BtnLink to="/newproject" text="Criar projeto"/>
+            <img src={savings} alt="costs"/>
+
+        </section>
     );
 }
 
