890d55785a6faf9f81caec8104415b8daac8c8d6
Edinaldo Fernandes Cardoso Santos <fernandescro5@gmail.com>
2022-02-12 04:18:10 -0300

    Aula26-Dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,9 @@
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
+import Container from '../layout/Container'
+import BtnLink from '../layout/BtnLink'
 
+import styles from './Projects.module.css'
 
 function Projects() {
 
@@ -12,9 +15,15 @@ function Projects() {
     } 
     
     return (
-        <div>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
             <h1>Projects</h1>
+            <BtnLink to="/newproject" text="Criar projeto"/>
+            </div>
             {message && <Message  type="success" msg={message}/>}
+            <Container customClass='start'>
+             <p>Projetos...</p>
+            </Container>
         </div>
     );
 }
