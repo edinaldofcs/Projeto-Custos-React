984deb30361e77c5e12c1a8d9d65b024dcb3478b
Edinaldo Fernandes Cardoso Santos <fernandescro5@gmail.com>
2022-02-13 18:43:24 -0300

    Aula32-Project-Update

--- src/components/pages/SingleProject.js
+++ src/components/pages/SingleProject.js
@@ -4,13 +4,16 @@ import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Loading from "../layout/Loading";
 import Container from '../layout/Container';
+import ProjectForm from '../project/ProjectFrom';
+import Message from '../layout/Message';
 
 function SingleProject() {
 
     const { id } = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
-
+    const [message, setMessage] = useState('');
+    const [type, setType] = useState('');
 
     useEffect(() => {
 
@@ -36,12 +39,38 @@ function SingleProject() {
         setShowProjectForm(!showProjectForm)
     }
 
+    function editPost(project) {
+        if (project.budget < project.cost) {
+            //mensagem
+            setMessage('O orçamento deve ser maior que o custo')
+            setType('fail')
+            return false
+        }
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(data)
+                setShowProjectForm(false)
+                setMessage('Projeto atualizado com sucesso!')
+                setType('success')
+            })
+            .catch(err => console.log(`singleProject-Erro-Update: ${err}`))
+    }
+
     return (
         <>
             {project.name ? (
 
                 <div className={styles.project_details}>
                     <Container customClass="column">
+                        {message && <Message type={type} msg={message}/>}
                         <div className={styles.details_container}>
                             <h1>Projeto: {project.name}</h1>
                             <button className={styles.btn} onClick={toggleProjecForm}>
@@ -58,11 +87,15 @@ function SingleProject() {
                                     <p>
                                         <span>Total utilizado: </span>R${project.cost}
                                     </p>
-                                </div>    
+                                </div>
                             ) : (
                                 <div className={styles.project_info}>
-                                    <p>Form</p>
-                                </div>  
+                                    <ProjectForm
+                                        handleSubmit={editPost}
+                                        btntext='Concluir edição'
+                                        projectData={project}
+                                    />
+                                </div>
                             )}
                         </div>
                     </Container>
